0e6d94dc3516eb086d6fc1301401192f81b825a2
Nidhi Patel <85288488+nidhipatel1211@users.noreply.github.com>
2023-06-28 18:54:27 -0400

    Update DependencyInjectionLauncherApplication.java

--- src/main/java/com/example/demo/a1/DependencyInjectionLauncherApplication.java
+++ src/main/java/com/example/demo/a1/DependencyInjectionLauncherApplication.java
@@ -1,25 +1,44 @@
 package com.example.demo.a1;
 
-
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class buisnessclass {
+	@Autowired
+	dependency1 dep1;
+	@Autowired
+	dependency2 dep2;
+
+	public String toString() {
+		return "Using" + dep1 + "and" + dep2;
+	}
+}
+
+@Component
+class dependency1 {
+
+}
+
+@Component
+class dependency2 {
+
+}
 
 @Configuration
 @ComponentScan
 public class DependencyInjectionLauncherApplication {
-	
+
 	public static void main(String[] args) {
 
-		try (var context = 
-				new AnnotationConfigApplicationContext
-					(DependencyInjectionLauncherApplication.class)) {
-			Arrays.stream(context.getBeanDefinitionNames())
-			.forEach(System.out::println);
-			
-
+		try (var context = new AnnotationConfigApplicationContext(DependencyInjectionLauncherApplication.class)) {
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+			System.out.println(context.getBean(buisnessclass.class));
 		}
 	}
 }
